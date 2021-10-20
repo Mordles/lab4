bd1c7ac3228b9dbbd32c62566eb751b2bb5a0f75
Mordyg <91845645+Mordles@users.noreply.github.com>
2021-10-20 05:54:48 +0000

    g

--- select_led.py
+++ select_led.py
@@ -12,19 +12,15 @@ data = {"option":radio_button, "slider":slider_position}
 with open('selected_led.txt', 'w') as f:
   json.dump(data,f)
 
-print("""
-
-Content-type:text/html\n\n
-<html>
-<form action="/cgi-bin/select_led.py" method="POST">
-  <input type="radio" name="option" value="1"> LED 1 <br>
-  <input type="radio" name="option" value="2"> LED 2 <br>
-  <input type="radio" name="option" value="3"> LED 3 <br>
-  <input type="submit" value="Submit"> <br>
-  <input type="range" name="slider" min="0" max="100" value="%s"><br> % slider_position
-  <input type="submit" value="Change selected LED brightness">
-</form>
-Brightness = %s % slider_position
-</html>
-
-""")
+print('Content-type:text/html\n\n')
+print('<html>')
+print('<form action="/cgi-bin/select_led.py" method="POST">')
+print('<input type="radio" name="option" value="1"> LED 1 <br>')
+print('<input type="radio" name="option" value="2"> LED 2 <br>')
+print('<input type="radio" name="option" value="3"> LED 3 <br>')
+print('<input type="submit" value="Submit"> <br>')
+print('<input type="range" name="slider" min="0" max="100" value="%s"><br>' % slider_position)
+print('<input type="submit" value="Change selected LED brightness">')
+print('</form>')
+print('Brightness = %s' % slider_position)
+print('</html>')  
--- select_led_background.py
+++ select_led_background.py
@@ -15,31 +15,28 @@ GPIO.setup(led2, GPIO.OUT)
 GPIO.setup(led3, GPIO.OUT)
 
 pwm1 = GPIO.PWM(led1, 100)
-pwm1.start(0)
+pwm1.start(50)
 
 pwm2 = GPIO.PWM(led2, 100)
-pwm2.start(0)
+pwm2.start(50)
 
 pwm3 = GPIO.PWM(led3, 100)
-pwm3.start(0)
+pwm3.start(50)
 
 while True:
   with open('selected_led.txt', 'r') as f:
     data = json.load(f)
-  dutyCycle = float(data['slider'])
+    dutyCycle = float(data['slider'])
 
-  if (data['option']==1):
+  if (int(data['option']==1)):
     pwm1.ChangeDutyCycle(dutyCycle)
-  elif (data['option']==2):
+
+  elif (int(data['option']==2)):
     pwm2.ChangeDutyCycle(dutyCycle)
-  elif (data['option']==3):
+
+  elif (int(data['option']==3)):
     pwm3.ChangeDutyCycle(dutyCycle)
+
   time.sleep(.1)
 
-
-  
-
-
-
-
-
+ 
